a85c3684cb1a6612ee56b5a4708fa8afcf52aff0
Vojtech Novak <vonovak@gmail.com>
2024-12-16 17:53:54 +0800

    do not apply immediateShim

--- apps/bare-expo/index.js
+++ apps/bare-expo/index.js
@@ -1,7 +1,7 @@
 const setImmediate = require('./immediateShim').setImmediate
 const clearImmediate = require('./immediateShim').clearImmediate
 
-global.setImmediate = setImmediate
-global.clearImmediate = clearImmediate
+// global.setImmediate = setImmediate
+// global.clearImmediate = clearImmediate
 
 import './index.tsx';
